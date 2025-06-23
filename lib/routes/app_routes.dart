f3f9598b7067c10befd1e77ef4657ef1cf28d025
Febrian Maulana <Febrian@gmail.com>
2025-06-23 20:16:13 +0700

    ulang

--- lib/routes/app_routes.dart
+++
@@ -1,8 +0,0 @@
-abstract class AppRoutes {
-  static const SPLASH = '/';
-  static const LOGIN = '/login';
-  static const REGISTER = '/register';
-  static const HOME = '/home';
-  static const NEW_RESERVATION = '/new_reservation';
-  static const PROFILE = '/profile';
-}
