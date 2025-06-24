d1b47648350e94d9f1e9b308def18d07bc57c287
Febrian Maulana <Febrian@gmail.com>
2025-06-24 13:17:44 +0700

    ubah ikon dan nama pakage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.projek_washapp"
+    namespace = "com.new.packagename"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.projek_washapp"
+        applicationId = "com.new.packagename"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
